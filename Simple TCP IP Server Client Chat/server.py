8ecb6c5944ee7c399743e5fd733f19fd5744e229
DrMayhem <96137905+DrMayhem@users.noreply.github.com>
2022-12-01 14:29:41 +0530

    Create server.py

---
+++ Simple TCP IP Server Client Chat/server.py
@@ -0,0 +1,27 @@
+import socket
+
+
+def server_program():
+    # get the hostname
+    host = socket.gethostname()
+    port = 5000  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+    server_socket.listen(2)
+    conn, address = server_socket.accept() 
+    print("Connection from: " + str(address))
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  
+
+    conn.close()  # close the connection
+
+
+if __name__ == '__main__':
+    server_program()
